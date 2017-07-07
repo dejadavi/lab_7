f846927e0d25adeb7add2562f54fe7649843dfb5
Desmond Davis <Dez@Desmonds-MacBook-Air.local>
2017-07-07 09:43:20 -0400

    Cleaned up

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ////Item constructor
-function item(name, price){
+function Item(name, price){
   
   this.name=name;
   this.price=price;
@@ -13,85 +13,76 @@ function item(name, price){
 };
 
 ////Shopping cart constructor
-function shoppingCart(){
+function ShoppingCart(){
     //list of items
     this.items=[];
     this.total=0;
   	
 
-       /////print out your current list
+    /////print out your current list
     this.printList= function(){
         this.items.forEach(function(thing){
-        console.log(thing.name+":"+thing.quantity+'\n');
+        console.log(thing.name+":"+thing.quantity+"\r");
        })
    
      } 
     
-    
+    ////Builds on printlist, prints curent status of cart
     this.cartCheck=function(){
         console.log("*******Total Cart*******");
         this.printList();
-        console.log("Cart total:$ "+this.total+"/n/n");
+        console.log("Cart total:$ "+this.total+"\r\r");
       	
     }
-
+		
+    ////Close out and payup
     this.checkOut=function (){
        console.log("*****Checking Out*******");
        this.printList();
-       console.log("Total due:"+this.total+'\n'+"Cash or credit?");
-      console.log("*****************");
-       return false;
+       console.log("Total due:"+this.total+"\r"+"Cash or credit?");
+       console.log("*****************");
+       //return false;
     }
     
     
     ////check items list and update number if appropiate
     this.addItem=function(itemToAdd){
-        var _contained=false;
-        var _objectCheck=false;
-      	var _location;
+      
+        var  cartItemsList=this.items;
+        var _objectCheck=Object.getPrototypeOf(itemToAdd)==Object.getPrototypeOf(Item);
+      	
        ////If function is not passed an item-type object, console log an error
         if(_objectCheck){
             console.log("Invlaid input type, try again.")
+            return false;
+            }
        ////Else if f object type is item, use for loop to check if any onject with same name are already in list
-        } else if (this.items.length===0){
-            _contained=false;
-          	//console.log("Empty cart");
-        } else {
-      	    ////For loops through object items list and 
-                for(var i=0;i<this.items.length;i++){
-                ////If name of item to add equals any name of object in items list, increase quantity of that item
-                    if(this.items[i].name===itemToAdd.name){  
-                      _location=i;
-                      _contained=true;
-                     
-                } 
-                
-             } ////Exit for loop
-                  
-         }//exit else
-      
-      if(_contained){
-				
+        if (cartItemsList.length>0){
+          
+          for(var i=1;i<cartItemsList.length;i++){
+          //  cartItemsList.(function(listItem,cartIndex){ 
+                if(cartItemsList[i].name==itemToAdd.name){
+                    cartItemsList[i].quantity++;
+                    this.total+=listItem.price;
+                    this.cartCheck();
+                    return true;
+                }
+            } 
+       } 
+       cartItemsList.push(itemToAdd);
        this.total+=itemToAdd.price;
-       this.items[_location].quantity++;
-       console.log("Added another "+itemToAdd.name+" to your cart.");
-       } else if (!_contained){
-               this.items.push(itemToAdd);
-               this.total+=itemToAdd.price;
-               console.log("Added a\(n\) "+itemToAdd.name);
-       } else {console.log("Error, check input")} ////Exit Else loop   
-    
-    this.cartCheck();
+        console.log("You added 1 "+itemToAdd.name);
+        this.cartCheck();
     }////Exit addItem
-  
 };////End shoppingCart constructor
   
   
   
-var Apple =new item("Apple", .50);
-var Pear =new item("Pear", .75);
-var desCart= new shoppingCart();
+var Apple =new Item("Apple", .50);
+var Pear =new Item("Pear", .75);
+var desCart= new ShoppingCart();
 desCart.addItem(Apple);
 desCart.addItem(Apple);
+desCart.addItem(Pear);
 desCart.checkOut();
 //console.log(Apple.price);
