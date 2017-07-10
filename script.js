c75ff2a5a89ec1f66fdec79363c9e011f9e5c3f7
Desmond Davis <Dez@Desmonds-MacBook-Air.local>
2017-07-10 09:20:44 -0400

    5th commit

--- script.js
+++ script.js
@@ -5,35 +5,21 @@ function Item(name, price,id){
   this.price=price;
   this.quantity=1;
   this.id=id;
+  this.outputRow=undefined;
 
-  var icon=document.
-  
-  this.scanItem= function(){
-      console.log("Item Name:"+this.name+"\n"+"Item Price:"+this.price);
-  }
-  
-  
+    
 };
 
-function store(){
+function Store(){
 
-this.inventory=[];
-this.inventory.Apple =new Item("Apple", .50,0);
-this.inventory.Pear =new Item("Pear", .75),1;
-this.inventory.Butter =new Item("Butter", 3.00,2);
-
-forEach(function(thing, index){
-var button=document
-
-
-})
-
+this.inventory={};
+this.inventory["0"]=new Item("Apple", .50,0);
+this.inventory["1"]=new Item("Orange", .50,0);
+this.inventory["2"]=new Item("Pear", .75,1);
+this.inventory["3"]= new Item("Grape", 3.00,2);
+};
 
 ////populate dom with buttons, each with id from id
-
-};
-
-
 
 
 
@@ -43,10 +29,11 @@ function ShoppingCart(){
     //list of items
     this.items=[];
     this.total=0;
+    this.rowNum=0;
   	
 
     /////print out your current list
-    this.printList= function(){
+    this.print= function(){
         this.items.forEach(function(thing){
         console.log(thing.name+":"+thing.quantity+"\r");
        })
@@ -72,46 +59,151 @@ function ShoppingCart(){
     
     
     ////check items list and update number if appropiate
-    this.addItem=function(itemToAddId){
+    this.addItem=function(addId){
       
         var  cartItemsList=this.items;
-        var _objectCheck=Object.getPrototypeOf(itemToAdd)==Object.getPrototypeOf(Item);
+        //var _objectCheck=Object.getPrototypeOf(desStore[addId])==Object.getPrototypeOf(Item);
       	
        ////If function is not passed an item-type object, console log an error
-        if(_objectCheck){
+        if(false){
             console.log("Invlaid input type, try again.")
             return false;
             }
        ////Else if f object type is item, use for loop to check if any onject with same name are already in list
         if (cartItemsList.length>0){
           
-          for(var i=1;i<cartItemsList.length;i++){
+          for(var i=0;i<cartItemsList.length;i++){
           //  cartItemsList.(function(listItem,cartIndex){ 
-                if(cartItemsList[i].id==itemToAdd.id){
+                if(cartItemsList[i].id==addId){
                     cartItemsList[i].quantity++;
-                    this.total+=listItem.price;
-                    this.cartCheck();
+                    this.total+=cartItemsList[i].price;
+                    console.log(cartItemsList[i]);
+                    console.log("You added 1 more "+cartItemsList[i]);
                     return true;
                     ///update dom grid element
                 }
             } 
        }
-       //create dom grid element 
-       cartItemsList.push(itemToAdd);
+      
+       var itemToAdd=desStore.inventory[addId];
+       itemToAdd.outputRow=this.rowNum;
+       console.log(itemToAdd);
        this.total+=itemToAdd.price;
+       cartItemsList.push(itemToAdd);
+      
        console.log("You added 1 "+itemToAdd.name);
-       this.cartCheck();
+
+        var total=document.getElementById("total");
+    total.textContent=desCart.total;
+        
+       
+    //    var icon=document.createElement("div");
+    //    icon.setAttribute("class", "foodIcon");
+    //    icon.textContent=itemToAdd.name;
+       
+    //    var parent=document.getElementById("panel2");
+    //    parent.appendChild(icon);
+    //    this.rowNum++;
        return true;
        
     }////Exit addItem
 };////End shoppingCart constructor
   
+  var dragged=undefined;
+
+///Picked up object events
+
+
+
+
+ document.addEventListener("drag", function( event ) {
+     
+  }, false);
+
+  document.addEventListener("dragstart", function(event){
+    //triggered once, start of drag
+    dragged=event.target.cloneNode(false);
+    event.target.style.opacity=.5;
+    var potential=document.getElementsByClassName("dropable");
+    for(var i=0;i<potential.length;i++){
+         potential[i].style.backgroundColor="blue";
+         potential[i].style.opacity=.5;
+     }
+
+
+  }, false);
+
+
+  document.addEventListener("dragend", function(event){
+ 
+}, false);
+
+//drop target events
+
+ 
+
+document.addEventListener("dragover",function(event){
+    //ms delay, little bit of hover. allows drop to happen
+            event.preventDefault();
+
+    
+},false);
+
+document.addEventListener ("dragenter", function(event){
+    //enter potential drag zone
+    if(event.target.className=="dropable"){
+            event.target.style.background="purple";
+    }
+}, false);
+
+document.addEventListener ("dragleave", function(event){
+    //leave potential drop zone
+            
+}, false);
+
+document.addEventListener("drop", function(event){
+    event.preventDefault();
+
+    if(event.target.className=="dropable"){
+           event.target.classList.add("dropped");
+            event.target.classList.remove("dropable");
+            event.target.style.opacity=null;
+            event.target.style.background=null;
+            event.target.dataset.id=dragged.id;
+            event.target.onclick=desCart.addItem(event.target.dataset.id);
+           event.target.textContent=desStore.inventory[event.target.dataset.id].name;
+      
+
+    }  
+    
   
-  
+        
+        var potential=document.getElementsByClassName("dropable");
+    for(var i=0;i<potential.length;i++){
+        if(potential[i].className=="dropable"){
+         potential[i].style.backgroundColor=null;
+         potential[i].style.opacity=null;}
+         
+   
+     }
+    //     dragged.style.opacity=.5;
+      
+    
+}, false);
+
+
+
+
+
+
 
 var desCart= new ShoppingCart();
+var desStore= new Store();
+
+
+
 // desCart.addItem(Apple);
 // desCart.addItem(Apple);
 // desCart.addItem(Pear);
-desCart.checkOut();
+//desCart.checkOut();
 //console.log(Apple.price);
